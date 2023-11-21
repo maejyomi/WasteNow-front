cf6f984b74094aa888501fde90d15db9602d3624
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-11-21 17:44:39 +0900

    검색페이지 구현
    - 검색 페이지 완료

--- src/comm/MySelect.js
+++ src/comm/MySelect.js
@@ -4,7 +4,7 @@ const MySelect = ({ id, optionItem, handleChange }) => {
         return <option key={idx} value={item} className="bg-white">{item}</option>
     })
     return (
-        <select onChange={handleChange} className="w-[100%] h-[3rem] border-none bg-[#EDEDED] rounded-lg px-5 focus:border-[#5586f8]">
+        <select onChange={handleChange} className="w-[100%] h-[3rem] border-none bg-[#EDEDED] rounded-lg px-5 focus:border-[#5586f8] focus:ring-[#5586f8] ">
             <option value='' className="bg-white">선택</option>
             {options}
         </select>
--- src/components/Search.js
+++ src/components/Search.js
@@ -9,20 +9,22 @@ const Search = () => {
     const [sido, setSido] = useState();
     const [cate, setCate] = useState();
     const keyword = useRef();
-    const [searchData, setSearchData] = useState([]);
+    const [searchData, setSearchData] = useState();
     const [tags, setTags] = useState();
 
 
-    const testData = { // 테스트용 데이터
-        cate: "가구류",
-        id: 1557,
-        manager: "부산광역시 강서구청",
-        name: "책상",
-        pay: "유료",
-        price: 8000,
-        sido: "강서구",
-        size: "양수(120cm 이상)"
-    }
+    const testData = [// 테스트용 데이터
+        {
+            cate: "가구류",
+            id: 1557,
+            manager: "부산광역시 강서구청",
+            name: "책상",
+            pay: "유료",
+            price: 8000,
+            sido: "강서구",
+            size: "양수(120cm 이상)"
+        },
+    ];
 
     const sidoChange = (e) => {
         setSido(e.target.value);
@@ -33,26 +35,48 @@ const Search = () => {
     }
 
     const submitBtn = (e) => {
-        if (!sido || !cate || keyword.current.value == '') {
+        if (!sido || !cate || keyword.current.value === '') {
             alert("모든 항목을 입력해주세요.");
             return;
         }
 
         let url = `http://10.125.121.214:8080/api/search?sido=${sido}&cate=${cate}&keyword=${keyword.current.value}`;
-
+        
         fetch(url)
             .then(resp => resp.json())
             .then(data => setSearchData(data))
             .catch(err => console.log(err));
+        
+
+        // setSearchData(testData); // 테스트 코드
+
     }
 
     useEffect(() => {
         console.log(searchData);
-        setTags(searchData.map((item, idx)=>{
+        if(!searchData) return;
+
+        // 데이터 없을 때 처리 (나중에 수정)
+        if(searchData.length == 0){
+            setTags(
+                <div>
+                    <p className="">데이터가 없습니다.</p>
+                </div>
+            
+            )
+            return;
+        }
+
+        setTags(searchData.map((item, idx) => {
             return (
-                <div key={idx} className="border-2 border-[#5586f8] rounded-lg shadow-lg h-[15rem] p-3">
-                    <h1 className="text-center text-xl font-bold mb-[2rem]">{item.name}</h1>
-                    <p className="mb-1">규격 : {item.size}</p>
+                <div
+                    key={idx}
+                    className="border-2 border-[#5586f8] rounded-lg shadow-lg h-[15rem] md:h-[18rem] lg:h-[15rem] p-5">
+                    <h1 className="text-center text-xl font-bold mb-[1rem]">
+                        {item.name}
+                    </h1>
+                    <hr />
+                    <p className="mt-[1rem] mb-1 ">규격 : {item.size}</p>
                     <p className="mb-1">유무료 : {item.pay}</p>
                     <p className="mb-1">수수료 : {item.price}</p>
                     <p>관리기관 : {item.manager}</p>
@@ -76,7 +100,7 @@ const Search = () => {
                     <input type="text"
                         ref={keyword}
                         placeholder="키워드를 입력해주세요"
-                        className="w-full h-[3rem] px-4 rounded-lg border-none focus:border-[#5586f8] bg-[#EDEDED] " />
+                        className="w-full h-[3rem] px-4 rounded-lg border-none focus:border-[#5586f8] focus:ring-[#5586f8] bg-[#EDEDED] " />
                 </div>
                 <div>
                     <button
@@ -86,7 +110,7 @@ const Search = () => {
                     </button>
                 </div>
             </div>
-            <div className="mt-[4rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10">
+            <div className="mt-[4rem] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-10">
                 {tags}
             </div>
 
