4a6b3176d20e9ce7fe668b38f38ffaee0b8f5dfe
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-11-23 19:45:59 +0900

    글쓰기 구현

--- src/components/board/NanoomPost.js
+++ src/components/board/NanoomPost.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useRef, useState } from 'react'
 import CustomSelect from '../CustomSelect';
 import axios from 'axios';
-
+import { Link, useNavigate } from 'react-router-dom';
 
 const NanoomPost = () => {
+
   const name = localStorage.getItem("username");
 
   const selCate = ["가구류", "가전제품류", "기타", "생활용품류"];
   const selNanoom = ["나눔중", "나눔완료"]
+
+  const navigate = useNavigate();
 
   const [nanoom, setNanoom] = useState();
   const [cate, setCate] = useState();
@@ -57,8 +60,25 @@ const NanoomPost = () => {
 
   useEffect(() => {
     // console.log(wasteName);
-
   }, [wasteName])
+
+  // 이미지 업로드
+  const [imgUrl, setImgUrl] = useState();
+
+  const handleImage = (e) => {
+    const file = e.target.files[0]
+    
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      setImgUrl(e.target.result);
+    }
+    reader.readAsDataURL(file);
+  }
+
+  useEffect(()=>{
+    // console.log(imgUrl);
+  },[imgUrl])
+
 
   // 게시글 등록
   const postClick = () => {
@@ -69,8 +89,33 @@ const NanoomPost = () => {
         return;
     }
 
+    if(!imgUrl){
+      alert("사진을 선택해주세요");
+      return;
+    }
+    const postData = {
+      name: selName,
+      cate: cate,
+      username: name,
+      title: title.current.value,
+      content: content.current.value,
+      image: imgUrl,
+      tag: nanoom,
+      count: 0,
+    }
+
+    console.log(postData)
+
     // 모든 항목이 있을 때 fetch하기
-    
+    axios.post("http://10.125.121.214:8080/api/user/nowWrite",postData)
+                .then((resp)=>{
+                    navigate("/nanoomlist");
+                })
+                .catch((err) =>{
+                    console.log(err);
+                    alert("게시글 등록 실패");
+                });
+
   }
 
   return (
@@ -96,13 +141,12 @@ const NanoomPost = () => {
             <textarea id="content" ref={content} rows="4" className="block p-2.5 w-[100%] text-sm resize-none h-full text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 " placeholder="내용을 자유롭게 작성해보세요"></textarea>
           </div>
           <div className='flex justify-between'>
-            <input type='file' />
+            <input type='file' onChange={handleImage} id='img'/>
             <p>작성자: {name}</p>
           </div>
         </div>
-
-
       </div>
+      <button className='flex m-auto mt-[1rem]'><Link to='/nanoomlist'>목록으로</Link></button>
     </div>
   )
 }
