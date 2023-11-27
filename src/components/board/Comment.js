fd3d759b1bbfa8faf7df5aac68cf0a3ea1f9c848
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-11-27 18:04:15 +0900

    댓글 추가 기능 구현

--- src/components/board/Comment.js
+++ src/components/board/Comment.js
@@ -1,21 +1,58 @@
 import axios from "axios";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Comment = ({ postId }) => {
-  // console.log(postId);
   const username = localStorage.getItem("username");
-
-  // test
-  localStorage.setItem("username","user4");
-
   const comm = useRef();
+  const [commData, setCommData] = useState();
+  const [commTag, setCommTag] = useState();
+
+  useEffect(() => {
+    const url = `http://10.125.121.214:8080/api/user/nowBoard?postId=${postId}`;
+
+    axios.get(url)
+      .then(resp => {
+        setCommData(resp.data.comment);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+
+  }, [])
+
+  useEffect(() => {
+    if (!commData) return;
+
+    console.log(commData);
+    setCommTag(commData.map((item) => {
+      return (
+        <div className="flex justify-between" key={item.commentId}>
+          <div className="flex gap-2">
+            <p>[ {item.username} ]</p>
+            <p>{item.commContent}</p>
+          </div>
+          {
+            username === item.username
+            ? <div>
+                수정 삭제
+              </div>
+            : ""
+          }
+        </div>
+      )
+    }))
+
+  }, [commData])
+
+
+  // 댓글 작성 등록
   const commSubmit = () => {
     if (comm.current.value === "") {
       alert("내용을 입력해주세요");
       comm.current.focus();
       return;
     }
-    // console.log(comm.current.value);
 
     const commPostData = {
       postId: postId,
@@ -29,17 +66,22 @@ const Comment = ({ postId }) => {
       .post(url, commPostData)
       .then((resp) => {
         console.log("성공");
+        setCommData([...commData, commPostData]);
+        comm.current.value = "";
+
       })
       .catch((err) => {
         console.log("댓글 등록 실패");
       });
+
+
   };
 
   return (
     <div className="flex flex-col w-full max-w-[800px] m-auto  mt-[1rem] bg-white shadow-lg rounded-lg px-[3rem] py-[2rem]">
       <div className="grow flex flex-col">
-        <div className="w-full">
-          <h3>{username}</h3>
+        <div className="w-full mb-2">
+          <h3 className="font-bold">{username}</h3>
           <input
             type="text"
             ref={comm}
@@ -53,12 +95,11 @@ const Comment = ({ postId }) => {
             등록
           </button>
         </div>
-          <div className="">
-            <p>test</p>
-            <p>test</p>
-            <p>test</p>
-            
-          </div>
+        <div className="">
+          {
+            commTag
+          }
+        </div>
       </div>
     </div>
   );
--- src/components/board/NanoomDetail.js
+++ src/components/board/NanoomDetail.js
@@ -8,10 +8,13 @@ const NanoomDetail = () => {
     const postId = useParams().postId;
     const username = localStorage.getItem("username");
 
+    // console.log(username);
     // console.log(postId);
 
     const [detailData, setDetailData] = useState([]);
+
     const [detailTag, setDetailTag] = useState();
+
     // 테스트용 데이터
     const testData = [
         {
@@ -19,7 +22,7 @@ const NanoomDetail = () => {
             name: "소파",
             cate: "가구류",
             username: "maejyomi",
-            content: "1년 사용한 소파 무료나눔 합니다! 상태 좋습니다!! 영도구 ## 아파트 앞에서 가져가시면 됩니다!",
+            content: "1년 사용한 소파 무료나눔 합니다! 영도구 ## 아파트 앞에서 가져가시면 됩니다!",
             title: "소파 무료나눔 합니다",
             image: "이미지",
             count: 0,
@@ -28,26 +31,26 @@ const NanoomDetail = () => {
         }
     ]
 
-    
+
 
     // 제일 처음 데이터를 받아오는 부분
     useEffect(() => {
         const url = `http://10.125.121.214:8080/api/user/nowBoard?postId=${postId}`;
 
-        /*
+
         axios.get(url)
             .then(resp => {
-                console.log(resp.data);
-                setDetailData(resp.data);
+                //console.log(resp.data.board);
+                setDetailData(resp.data.board);
             })
             .catch(err => {
                 console.log(err);
             })
 
-        */
+
 
         // 테스트용
-        setDetailData(testData);
+        //setDetailData(testData);
     }, [])
 
     useEffect(() => {
@@ -63,12 +66,12 @@ const NanoomDetail = () => {
                             <p className="border-4 border-[#fafa98] rounded-full p-1 px-2 font-bold">#{item.name}</p>
                             {
                                 item.tag === "나눔중"
-                                ? <p className="border-4 border-[#bbf9ff] rounded-full p-1 px-2 font-bold">#{item.tag}</p>
-                                : <p className="border-4 border-[#bbf9ff] bg-[#bbf9ff] rounded-full p-1 px-2 font-bold">#{item.tag}</p>
+                                    ? <p className="border-4 border-[#bbf9ff] rounded-full p-1 px-2 font-bold">#{item.tag}</p>
+                                    : <p className="border-4 border-[#bbf9ff] bg-[#bbf9ff] rounded-full p-1 px-2 font-bold">#{item.tag}</p>
                             }
                         </div>
                     </div>
-                    <div className="flex h-[90%] gap-4">
+                    <div className="flex h-[80%] gap-4">
                         <div className="grow w-[30%]">
                             <img src={item.image} className="max-h-[240px] w-[100%] object-cover" />
                         </div>
@@ -76,13 +79,23 @@ const NanoomDetail = () => {
                         <div className="grow flex flex-col w-[70%]">
                             <div className="flex justify-between text-sm my-2">
                                 <p>{item.username}</p>
-                                <p>{item.createDate.slice(0,10)}</p>
+                                <p>{item.createDate.slice(0, 10)}</p>
                             </div>
                             <div className="">
                                 <p>{item.content}</p>
                             </div>
                         </div>
                     </div>
+                    {
+                        username === detailData[0]["username"]
+                            ?
+                            <div className="flex justify-end border-t-2 mt-2">
+                                <div className="mt-2">
+                                    수정 삭제
+                                </div>
+                            </div>
+                            : ""
+                    }
                 </div>
             )
         }))
@@ -92,13 +105,10 @@ const NanoomDetail = () => {
         <div className="grow px-[8rem] bg-gradient-to-b from-[#83a8ff] to-[#ffffff]">
             <div className="flex flex-col h-[50%] w-full max-w-[800px] m-auto mt-[4rem] bg-white shadow-lg rounded-lg px-[3rem] py-[2rem]">
                 {detailTag}
-                <div className="flex justify-end">
-                    수정 삭제 버튼 부분
-                </div>
             </div>
             <Comment postId={postId} />
-            <div  className="flex m-auto max-w-[800px] justify-center my-[1rem]">
-               <Link to='/nanoomlist'><p>목록으로</p></Link>
+            <div className="flex m-auto max-w-[800px] justify-center my-[1rem]">
+                <Link to='/nanoomlist'><p>목록으로</p></Link>
             </div>
         </div>
     )
