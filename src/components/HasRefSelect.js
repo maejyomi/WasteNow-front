87625ea38252197646edd58d9b79818c8f2d743c
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-11-27 18:03:44 +0900

    게시물 입력 업데이트
    - 시군구명, 사이즈도 선택하도록 수정

--- src/components/HasRefSelect.js
+++ src/components/HasRefSelect.js
@@ -1,14 +1,14 @@
 
-const CustomSelect = ({ optionItem, handleChange }) => {
+const HasRefSelect = ({refs, optionItem, handleChange }) => {
     const options = optionItem.map((item, idx)=>{
         return <option key={idx} value={item} className="bg-white">{item}</option>
     })
     return (
-        <select onChange={handleChange} className="w-[100%] h-[2.5rem] border-none bg-[#EDEDED] rounded-lg focus:border-[#5586f8] focus:ring-[#5586f8] ">
+        <select onChange={handleChange} ref={refs} className="w-[100%] h-[2.5rem] border-none bg-[#EDEDED] rounded-lg focus:border-[#5586f8] focus:ring-[#5586f8] ">
             <option value='' className="bg-white">선택</option>
             {options}
         </select>
     )
 }
 
-export default CustomSelect
+export default HasRefSelect
--- src/components/board/NanoomPost.js
+++ src/components/board/NanoomPost.js
@@ -2,36 +2,40 @@ import React, { useEffect, useRef, useState } from 'react'
 import CustomSelect from '../CustomSelect';
 import axios from 'axios';
 import { Link, useNavigate } from 'react-router-dom';
+import HasRefSelect from '../HasRefSelect';
 
 const NanoomPost = () => {
 
   const name = localStorage.getItem("username");
 
+  const selSido = ["강서구", "금정구", "남구", "동래구", "부산진구", "북구", "사상구", "서구", "수영구", "연제구", "영도구", "해운대구"];
   const selCate = ["가구류", "가전제품류", "기타", "생활용품류"];
-  const selNanoom = ["나눔중", "나눔완료"]
 
   const navigate = useNavigate();
 
-  const [nanoom, setNanoom] = useState();
+  const [sido, setSido] = useState();
   const [cate, setCate] = useState();
   const [wasteName, setWasteName] = useState([]);
   const [selName, setSelName] = useState();
-
+  const [wasteSize, setWasetSize] = useState([]);
+  const [selSize, setSelSize] = useState();
+  
   const title = useRef();
   const content = useRef();
-
-  const nanoomChange = (e) => {
-    if (e.target.value === "") { 
-      setNanoom(undefined);
-      return;
-    }
-    setNanoom(e.target.value);
-  }
+  const cateRef = useRef();
 
   // 카테고리 선택
+  const sidoChange = (e) => {
+    setWasteName([]);
+    setWasetSize([]);
+    cateRef.current.value = '';
+    setSido(e.target.value);
+  }
+
   const cateChange = (e) => {
     if (e.target.value === "") {
       setWasteName([]);
+      setWasetSize([]);
     }
     setCate(e.target.value);
 
@@ -40,27 +44,54 @@ const NanoomPost = () => {
   const wasteNameChange = (e) => {
     if(e.target.value === "") {
       setSelName(undefined);
+      setWasetSize([])
       return;
     }
     setSelName(e.target.value);
   }
 
+  const wasteSizeChange = (e) => {
+    setSelSize(e.target.value);
+    console.log(e.target.value);
+
+  }
+
   // 카테고리가 바뀌면 해당 폐기물 종류 이름 가져오기
   useEffect(() => {
     //console.log(cate);
-    if (!cate) return;
+    if (!cate || !sido) return;
 
-    let url = `http://10.125.121.214:8080/api/wastename?cate=${cate}`;
+    let url = `http://10.125.121.214:8080/api/wastename?sido=${sido}&cate=${cate}`;
+    console.log(url);
 
     fetch(url)
       .then(resp => resp.json())
-      .then(data => setWasteName(data))
+      .then(data => {
+        console.log(data);
+        setWasteName(data);
+      })
       .catch(err => console.log(err));
   }, [cate])
 
-  useEffect(() => {
-    // console.log(wasteName);
-  }, [wasteName])
+  // 폐기물 이름이 선택되면 해당 폐기물 이름의 사이즈 가져오기
+  useEffect(()=>{
+    if(!cate || !sido || !selName) return;
+
+    let url = `http://10.125.121.214:8080/api/wastesize?sido=${sido}&cate=${cate}&name=${selName}`;
+    console.log(url);
+
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        console.log(data);
+        setWasetSize(data);
+      })
+      .catch(err => console.log(err));
+
+  },[selName]);
+
+  
+
 
   // 이미지 업로드
   const [imgUrl, setImgUrl] = useState();
@@ -82,9 +113,9 @@ const NanoomPost = () => {
 
   // 게시글 등록
   const postClick = () => {
-
-    if (title.current.value === "" || content.current.value === "" || !nanoom ||
-      !cate || !selName) {
+    console.log()
+    if (title.current.value === "" || content.current.value === "" ||!sido ||
+      cateRef.current.value=="" || !selName || !selSize ) {
         alert("모든 항목을 입력해주세요");
         return;
     }
@@ -96,13 +127,15 @@ const NanoomPost = () => {
 
     // 댓글 관련 정보
     const postData = {
-      name: selName,
+      sido: selSido,
       cate: cate,
+      name: selName,
+      size: selSize,
       username: name,
       title: title.current.value,
       content: content.current.value,
       image: imgUrl,
-      tag: nanoom,
+      tag: "나눔중",
       count: 0,
     }
 
@@ -125,11 +158,12 @@ const NanoomPost = () => {
       <div className="max-w-[800px] m-auto flex flex-col h-[85%] w-full mt-[4rem]  bg-white shadow-lg rounded-lg px-[3rem] py-[2rem]">
         <h1 className='text-center font-bold text-2xl'>글쓰기</h1>
 
-        <div className='grid grid-cols-3 lg:grid-cols-6 gap-2 mt-[1.5rem]'>
-          <CustomSelect optionItem={selNanoom} handleChange={nanoomChange} />
-          <CustomSelect optionItem={selCate} handleChange={cateChange} />
+        <div className='grid grid-cols-4 gap-2 mt-[1.5rem]'>
+          <CustomSelect optionItem={selSido} handleChange={sidoChange} />
+          <HasRefSelect optionItem={selCate} refs={cateRef} handleChange={cateChange} />
           <CustomSelect optionItem={wasteName} handleChange={wasteNameChange}/>
-          <div className='flex items-center col-span-2 lg:col-span-5 h-[3rem] mt-[0.5rem]'>
+          <CustomSelect optionItem={wasteSize} handleChange={wasteSizeChange} />
+          <div className='flex items-center col-span-3 h-[3rem] mt-[0.5rem]'>
             <input type='text' ref={title} placeholder='제목을 입력해주세요'
               className='w-full h-full border-none bg-[#ededed] rounded-lg' />
           </div>
